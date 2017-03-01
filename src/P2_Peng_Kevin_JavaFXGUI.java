9164570f666d7b50acf20dad3b98db780c284448
kpeng356 <kpeng356@chs.win.fuhsd.org>
2017-03-01 10:13:38 -0800

    Started adding image to JavaXGUI

--- src/P2_Peng_Kevin_JavaFXGUI.java
+++ src/P2_Peng_Kevin_JavaFXGUI.java
@@ -4,11 +4,16 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Arc;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 /**
@@ -22,6 +27,9 @@ import javafx.stage.Stage;
  */
 
 public class P2_Peng_Kevin_JavaFXGUI extends Application {
+	
+	private ColorPicker bodyColorPicker;
+	private ColorPicker wheelColorPicker;
 
 	public void start(Stage stage) throws Exception {
 		stage.setTitle("GUI");
@@ -34,7 +42,7 @@ public class P2_Peng_Kevin_JavaFXGUI extends Application {
 		hBox.setSpacing(10);
 		Label label = new Label("Hello");
 		Button button = new Button("Button");
-		CheckBox checkbox = new CheckBox("I have reaad and agree to the terms");
+		CheckBox checkbox = new CheckBox("I have read and agree to the terms");
 		hBox.getChildren().addAll(label, button, checkbox);
 		root.setTop(hBox);
 		
@@ -42,13 +50,62 @@ public class P2_Peng_Kevin_JavaFXGUI extends Application {
 		slider.setPadding(new Insets(10));
 		root.setBottom(slider);
 		
+		bodyColorPicker = new ColorPicker(Color.RED);
+		root.setLeft(bodyColorPicker);
+		
+		wheelColorPicker = new ColorPicker(Color.BLACK);
+		root.setRight(wheelColorPicker);
+		
 		Group group = new Group();
 		Region region = new Region();
-		region.setMinSize(100, 100);
+		region.setMinSize(300, 300);
+		drawCar(group, 200, bodyColorPicker.getValue(), wheelColorPicker.getValue());
 		group.getChildren().add(region);
 		root.setCenter(group);
 		
 		stage.show();
+	}
+	
+	private void drawCar(Group group, int carWidth, Color bodyColor, Color wheelColor){
+		int carHeight = carWidth/2;
+		int wheelRadius = carWidth/8;
+		int wheelOffsetX = carWidth/3;
+		int wheelOffsetY = carWidth/4;
+		int windowOffset = carWidth/16;
+		int windowSeperatorWidth = carWidth/16;
+		
+		Color windowColor = Color.LIGHTBLUE;
+		
+		//bottom part
+		Rectangle bodyBottom = new Rectangle(group.prefWidth()/2 - CAR_WIDTH/2, group.getHeight()/2, CAR_WIDTH, CAR_HEIGHT/2);
+		bodyBottom.setFill(BODY_COLOR);
+		group.getChildren().add(bodyBottom);
+		
+		//top part
+		Arc bodyTop = new Arc(group.getWidth()/2 - CAR_WIDTH*0.125, group.getHeight()/2, CAR_WIDTH*0.375, CAR_HEIGHT/2, 0, 180);
+		bodyTop.setFill(BODY_COLOR);
+		group.getChildren().add(bodyTop);
+		
+		//window
+		Arc window = new Arc(group.getWidth()/2 - CAR_WIDTH*0.125, group.getHeight()/2, CAR_WIDTH*0.375 - WINDOW_OFFSET, CAR_HEIGHT/2 - WINDOW_OFFSET, 0, 180);
+		window.setFill(WINDOW_COLOR);
+		group.getChildren().add(window);
+		
+		//seperate the front and back window
+		Rectangle windowSeperator = new Rectangle(group.getWidth()/2 - CAR_WIDTH*0.125 - WINDOW_SEPARATOR_WIDTH, group.getHeight()/2 - CAR_HEIGHT/2 + WINDOW_OFFSET/2, WINDOW_SEPARATOR_WIDTH, CAR_HEIGHT/2 - WINDOW_OFFSET/2);
+		windowSeperator.setFill(BODY_COLOR);
+		group.getChildren().add(windowSeperator);
+		
+		//left wheel
+		Circle wheelLeft = new Circle(group.getWidth()/2 - WHEEL_OFFSET_X, group.getHeight()/2 + WHEEL_OFFSET_Y, WHEEL_RADIUS);
+		wheelLeft.setFill(WHEEL_COLOR);
+		group.getChildren().add(wheelLeft);
+		
+		//right wheel
+		Circle wheelRight = new Circle(group.getWidth()/2 + WHEEL_OFFSET_X, group.getHeight()/2 + WHEEL_OFFSET_Y, WHEEL_RADIUS);
+		wheelRight.setFill(WHEEL_COLOR);
+		group.getChildren().add(wheelRight);
+		
 	}
 
 	public static void main(String[] args) {
